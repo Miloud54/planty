d8cc1cfaa818becd277d787c16860196e5b658b0
Emilie Didier <emilie.didier97@gmail.com>
2024-12-09 18:57:44 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "pages#home"
+  get "dashboard" => "pages#dashboard", as: :dashboard
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :plants do
+    resources :bookings, only: %i[new create show] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+  end
+
+  resources :bookings, only: %i[delete]
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
