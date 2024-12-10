db7e1b197e725afae5e12223c2b00bf557b66c82
Emilie Didier <emilie.didier97@gmail.com>
2024-12-10 18:00:06 +0100

    pas réussi

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-// If using flatpickr
-import flatpickr from "flatpickr";
-import { Application } from "@hotwired/stimulus"
-import "./controllers"
-
-const application = Application.start()
-window.Stimulus = application
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,3 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/flatpickr.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "flatpickr"
-
